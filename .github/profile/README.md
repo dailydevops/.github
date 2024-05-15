3c19fa1f452df4daa8c0739b68440f2c76165985
Martin Stühmer <me@samtrion.net>
2024-05-16 00:08:33 +0200

    chore: Created README and script to provide the latest package infos

---
+++ .github/profile/README.md
@@ -0,0 +1,11 @@
+# Daily DevOps & .NET
+
+Welcome to our GitHub profile! We are a bunch of developers who love to share our knowledge and experience with the community. As part of our daily requirements, we create and maintain NuGet packages, mostly building blocks for .NET applications.
+
+## Overview - NuGet Packages
+
+We try to keep our packages as simple as possible, focusing on a single task or a small set of related tasks. This way, you can easily integrate them into your projects as accelerators or building blocks.
+
+<!-- packages:start -->
+123
+<!-- packages:end -->
---
+++ scripts/update-packages.ps1
@@ -0,0 +1,106 @@
+[CmdletBinding()]
+param (
+  [Parameter(Mandatory = $true)]
+  [string]$WorkingDirectory
+)
+
+function Get-Packages {
+  param (
+  )
+  $repositoryUrl = 'https://github.com/dailydevops'
+  $queryUrl = 'https://api-v2v3search-0.nuget.org/query?q=NetEvolve'
+  $response = Invoke-WebRequest -Uri $queryUrl
+  if ($response.statuscode -ne 200) {
+    Write-Error "Failed to get packages from $repositoryUrl"
+    return
+  }
+
+  $data = ConvertFrom-Json $response.Content
+  if ($data.totalHits -eq 0) {
+    Write-Error "No packages found at $repositoryUrl"
+    return
+  }
+
+  $result = @'
+
+<table>
+  <thead>
+    <tr>
+      <td><b>Package Name</b></td>
+      <td><b>Repository</b></td>
+      <td><b>Current Version</b></td>
+      <td><b>Downloads</b></td>
+    </tr>
+  </thead>
+  <tbody>
+
+'@
+
+  foreach ($package in ($data.data | Sort-Object -Property id)) {
+    if (-Not $package.projectUrl.StartsWith($repositoryUrl)) {
+      continue
+    }
+
+    $description = $package.description;
+
+    $result += @"
+    <tr>
+      <td><a href="https://www.nuget.org/packages/$($package.id)/"><b>$($package.title)</b></a></td>
+      <td><a href="$($package.projectUrl)">$($package.projectUrl)</a></td>
+      <td><a href="https://www.nuget.org/packages/$($package.id)/"><img src="https://img.shields.io/nuget/v/$($package.id)?logo=nuget" alt="Nuget"></a></td>
+      <td><a href="https://www.nuget.org/packages/$($package.id)/"><img src="https://img.shields.io/nuget/dt/$($package.id)?logo=nuget" alt="Nuget"></a></td>
+    </tr>
+    <tr>
+      <td colspan=4>$($description)</td>
+    </tr>
+
+"@
+  }
+
+  $result += @'
+  </tbody>
+</table>
+
+'@
+
+  return $result
+}
+
+function Update-Readme {
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $workingDirectoy,
+    [Parameter(Mandatory = $true)]
+    [string] $packagesContent
+  )
+
+  $tagStart = '<!-- packages:start -->'
+  $tagStartLength = $tagStart.Length
+  $tagEnd = '<!-- packages:end -->'
+
+  $readmeFiles = Get-ChildItem -Path $workingDirectory -Filter 'README.*' -Recurse
+
+  foreach ($readmeFile in $readmeFiles) {
+    $readmeContent = Get-Content -Path $readmeFile.FullName -Raw
+
+    $startIndex = $readmeContent.IndexOf($tagStart)
+    if ($startIndex -eq -1) {
+      continue
+    }
+    $startIndex += $tagStartLength
+    $endIndex = $readmeContent.IndexOf($tagEnd, $startIndex)
+
+    if ($endIndex -eq -1) {
+      continue
+    }
+
+    $readmeContent = $readmeContent.Replace($readmeContent.Substring($startIndex, $endIndex - $startIndex), $packagesContent)
+
+    Set-Content -Path $readmeFile.FullName -Value $readmeContent -NoNewline
+  }
+}
+
+$packages = Get-Packages
+if (![string]::IsNullOrWhiteSpace($packages)) {
+  Update-Readme -workingDirectoy $WorkingDirectory -packagesContent $packages
+}
